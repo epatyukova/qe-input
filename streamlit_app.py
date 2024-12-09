4db669f2b6e05961cfbc7e6d69b0d1a6e71b1ae3
epatyukova <patyukova@gmail.com>
2024-12-09 00:10:35 +0000

    message

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ if openai_api_key:
     # client = OpenAI(api_key=openai_api_key)
     if not llm_name:
         llm_name='gpt-4o'
-    client=ChatOpenAI(llm_name,openai_api_key,temperature=0)
+    client=ChatOpenAI(model_name=llm_name,openai_api_key=openai_api_key,temperature=0)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
