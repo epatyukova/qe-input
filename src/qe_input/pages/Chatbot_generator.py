3e2772bb8cf6b942cba64505a4def4a67cd3ce64
epatyukova <patyukova@gmail.com>
2025-02-06 13:06:21 +0000

    tests update

--- src/qe_input/pages/Chatbot_generator.py
+++ src/qe_input/pages/Chatbot_generator.py
@@ -15,7 +15,7 @@ if 'all_info' not in st.session_state.keys():
 
 with st.sidebar:
     llm_name_value = st.selectbox('assistant LLM', 
-                        ("gpt-4o", "gpt-4o-mini", 'gpt-3.5-turbo', 'llama-3.3-70b-versatile'), 
+                        ("gpt-4o", "gpt-4o-mini", 'gpt-3.5-turbo', 'llama-3.3-70b-versatile','mixtral-8x7b-32768','gemma2-9b-it'), 
                         index=None, 
                         placeholder='gpt-4o-mini')
 
